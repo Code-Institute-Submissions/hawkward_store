5dc80ab28ab86cae9c3b6a7ae9d760cc25485b91
johnsletvoldhafenrichter <john.hafenrichter@yahoo.com>
2020-09-15 14:11:07 +0200

    Added all products to base template, Added product view, added editing a product, added login required, Added cards to display products,

--- products/urls.py
+++ products/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.products, name='products'),
     path('add_product/', views.add_product, name='add_product'),
     path('add_category/', views.add_category, name='add_category'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, redirect, reverse
-from django.db.models import Q
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
 from .models import Product, Category
 from .forms import ProductForm, CategoryForm
@@ -8,13 +8,23 @@ from .forms import ProductForm, CategoryForm
 
 def products(request):
     """ Products page view """
-    return render(request, 'products/base.html')
+    products = Product.objects.all()
+    query = None
+    categories = None
+    sort = None
+    direction = None
 
+    context = {
+        'products': products,
+    }
+    return render(request, 'products/base.html', context)
+
+@login_required
 def add_product(request):
     if request.method == 'POST':
-        form = CategoryForm(request.POST, request.FILES)
+        form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            category = form.save()
+            product = form.save()
             return redirect(reverse('add_category'))
         else:
             return redirect(reverse('add_product'))
@@ -26,6 +36,7 @@ def add_product(request):
     }
     return render(request, template, context)
 
+@login_required
 def add_category(request):
     if request.method == 'POST':
         form = CategoryForm(request.POST, request.FILES)
@@ -40,4 +51,27 @@ def add_category(request):
     context = {
         'categoryForm': categoryForm,
     }
+    return render(request, template, context)
+    
+@login_required
+def edit_product(request, product_id):
+    """ Edit a product in the store """
+
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('products')
+        else:
+            return redirect('add_product')
+    else:
+        form = ProductForm(instance=product)
+
+    template = 'products/edit_product.html'
+    context = {
+        'productForm': form,
+        'product': product,
+    }
+
     return render(request, template, context)
