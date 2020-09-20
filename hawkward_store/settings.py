05caa2dbbfcd7af66ceb65e95fd1664851c1e75b
johnsletvoldhafenrichter <john.hafenrichter@yahoo.com>
2020-09-20 19:17:03 +0200

    added shopping bag app, started to create model for giftcard but deleted everything, changing giftcard model to be implemented at checkout

--- hawkward_store/settings.py
+++ hawkward_store/settings.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
     'home',
     'products',
     'users',
+    'shopping_bag',
 ]
 
 MIDDLEWARE = [
--- hawkward_store/urls.py
+++ hawkward_store/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('home.urls')),    
     path('products/', include('products.urls')),
     path('users/', include('users.urls')),
+    path('shopping_bag/', include('shopping_bag.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
