b80a0e6a8d4117176e3c7904b8f2393b093960e9
johnsletvoldhafenrichter <john.hafenrichter@yahoo.com>
2020-09-21 21:59:13 +0200

    Added context to shopping bag in order to view shopping bag, started working on shopping bag template.

---
+++ shopping_bag/contexts.py
@@ -0,0 +1,28 @@
+from django.conf import settings
+from products.models import Product
+from django.shortcuts import get_object_or_404
+
+def shopping_bag(request):
+
+    products = []
+    total_price = 0
+    total_products = 0
+    shopping_bag = request.session.get('shopping_bag', {})
+
+    for product_id, quantity in shopping_bag.items():
+        product = get_object_or_404(Product, pk=product_id)
+        total_price += quantity * product.price
+        total_products += quantity
+        products.append({
+            'product_id': product_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
+    context = {
+        'products': products,
+        'total_price': total_price,
+        'total_products': total_products,
+    }
+
+    return context
--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -1,16 +1,29 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from products.models import Product
 
 # Create your views here.
 
 def shopping_bag(request):
-    context = {
-        'shopping_bag': request.session['shopping_bag']
-    }
-    return render(request, 'shopping_bag/base.html', context)
+    print(request.session['shopping_bag'])
+    return render(request, 'shopping_bag/base.html')
 
 def add_item_to_bag(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
-    
+    quantity = 1
+    shopping_bag = request.session.get('shopping_bag', {})
 
-    return render(request, 'shopping_bag/base.html', context)
+    if request.POST:
+        quantity = int(request.POST['quantity'])
+        if product_id in list(shopping_bag.keys()):
+            shopping_bag[product_id] += quantity
+        else:
+            shopping_bag[product_id] = quantity
+
+    if product_id in list(shopping_bag.keys()):
+        shopping_bag[product_id] += quantity
+    else:
+        shopping_bag[product_id] = quantity
+    
+    request.session['shopping_bag'] = shopping_bag
+    print(request.session['shopping_bag'])
+    return redirect('shopping_bag')
