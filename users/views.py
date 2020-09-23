efe6d2e10db6c5e51b57ada70c1583529e03a12f
johnsletvoldhafenrichter <john.hafenrichter@yahoo.com>
2020-09-23 22:23:01 +0200

    Fixed same width and height for product cards, added some card css, updated requirements.txt, started showing giftcards on user profile, installed mathfilters

--- users/views.py
+++ users/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Giftcards
 
 # Create your views here.
 
 @login_required
 def users(request):
-    return render(request, 'users/base.html')
+    giftcards = Giftcards.objects.filter(user=request.user)
+    context = {
+        'giftcards': giftcards
+    }
+    return render(request, 'users/base.html', context)
